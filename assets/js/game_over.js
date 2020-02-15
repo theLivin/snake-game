ad152cdcbf06e15483dce1f939de161ade417536
BobBy <ayiteybobby@gmail.com>
2020-02-15 04:52:16 +0000

    Done!!

--- assets/js/game_over.js
+++ assets/js/game_over.js
@@ -9,7 +9,7 @@ var Game_Over = {
         this.add.button(0, 0, 'gameover', this.startGame, this);
 
         // Add text with information about the score from last game.
-        game.add.text(235, 350, "LAST SCORE", {font: "bold 16px sans-serif", fill: "#46c0f9", align: "center"});
+        game.add.text(235, 350, "YOU SCORED", {font: "bold 16px sans-serif", fill: "#46c0f9", align: "center"});
         game.add.text(350, 348, score.toString(), {font: "bold 20px sans-serif", fill: "#fff", align: "center"});
     },
 
